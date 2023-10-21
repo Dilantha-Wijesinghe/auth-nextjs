67b9685105efab2dca2bde481dcf12b458a7f231
Dilantha-Wijesinghe <wdilantha66@gmail.com>
2023-10-21 17:33:50 +0530

    edit dbconfig

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -2,7 +2,18 @@ import mongoose from 'mongoose';
 
 export async function connect() {
   try {
-    mongoose.connect(process.env.MONGO_URL!)
+    mongoose.connect(process.env.MONGO_URL!);
+    const connection = mongoose.connection;
+
+    connection.once('connected', () => {
+      console.log('MongoDB database connection established successfully!');
+    })
+
+    connection.on('error', (err) => {
+      console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err);
+      process.exit();
+
+    })
   } catch (error) {
     console.log('Something went wrong!');
     console.log(error);
