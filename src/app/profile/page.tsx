3844628d05703d3fbb99498284c14aeeb5432ce1
Dilantha-Wijesinghe <wdilantha66@gmail.com>
2023-10-24 20:22:06 +0530

    add button to profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -8,6 +8,8 @@ const page = () => {
         <h1>Profile</h1>
         <hr />
         <p>Profile page</p>
+        <hr />
+        <button className='bg-blue-800 mt-4 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded'>Logout</button>
       </div>
     </ClientOnly>
   );
