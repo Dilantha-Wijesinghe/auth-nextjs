29af1a828ac30d92e05ae0b7cd8845d4f4765ae9
Dilantha-Wijesinghe <wdilantha66@gmail.com>
2023-10-25 11:23:06 +0530

    add backend code to profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,26 @@
+"use client"
+import axios from 'axios';
+import Link from 'next/link';
 import React from 'react';
 import ClientOnly from '../../../components/ClientOnly';
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/router';
 
 const page = () => {
+
+  const router = useRouter(); // for redirecting
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logged out successfully");
+      router.push("/login");
+
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  }
+
   return (
     <ClientOnly>
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -9,7 +28,9 @@ const page = () => {
         <hr />
         <p>Profile page</p>
         <hr />
-        <button className='bg-blue-800 mt-4 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded'>Logout</button>
+        <button 
+          onClick={logout}
+          className='bg-blue-800 mt-4 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded'>Logout</button>
       </div>
     </ClientOnly>
   );
